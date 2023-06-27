dd4172ffd11773d265dc35dfc4cc87c24b48aa55
sydnief <sydniefarrell@gmail.com>
2023-06-26 22:16:15 -0400

    linked pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,15 +1,45 @@
+import React, { useState } from 'react';
 import './App.css';
 // Components
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// Pages
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import Portfolio from './components/pages/Portfolio';
+import Resume from './components/pages/Resume';
+
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Home />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Header />
+        <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
       </header>
       <main>
+      {renderPage()}
       </main>
       <footer className="App-footer">
         <Footer />
