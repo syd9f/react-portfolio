a27726c598dd4df388981a74d6102dcfbd5d08b5
sydnief <sydniefarrell@gmail.com>
2023-06-27 01:24:50 -0400

    added style to project

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Project = ({ title, image, description, github, deployed }) => {
     const imagePath = require(`../images/${image}`);
   return (
+    <section
+        style={{
+            background: "#FFE3FD",
+            border: "2px solid black",
+            padding: '20px'
+        }}
+    >
     <div>
       <h2>{title}</h2>
       <img src={imagePath} alt={title} height="200" />
@@ -10,6 +17,7 @@ const Project = ({ title, image, description, github, deployed }) => {
       <a href={github} target="_blank" rel="noopener noreferrer"><button>Github Repo</button></a>
       <a href={deployed} target="_blank" rel="noopener noreferrer"><button>Deployed Application</button></a>
     </div>
+    </section>
   );
 };
 
