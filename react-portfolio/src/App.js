564b668dbb9f403320641dc5937cf2e695cf30f4
sydnief <sydniefarrell@gmail.com>
2023-06-26 21:59:49 -0400

    updated links to switch between pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import Header from './components/Header'
-
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Render from './components/Render';
 
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
       <header className="App-header">
         <Header />
       </header>
+      <main>
+      <Render />
+      </main>
+      <footer className="App-footer">
+        <Footer />
+      </footer>
     </div>
   );
 }
--- react-portfolio/src/components/Navigation.js
+++ react-portfolio/src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Navigation() {
+export default function Navigation({ currentPage, handlePageChange }) {
   const linkStyle = { border: '1px black', padding: '5px' };
 
   return (
@@ -15,19 +15,39 @@ export default function Navigation() {
         }}
       >
         <div style={linkStyle}>
-          <a href="/">Home</a>
+          <a href="#home" 
+             onClick={() => handlePageChange('Home')} 
+             className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+          Home
+          </a>
         </div>
         <div style={linkStyle}>
-          <a href="/">About</a>
+          <a href="#about"
+             onClick={() => handlePageChange('About')} 
+             className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+          About
+          </a>
         </div>
         <div style={linkStyle}>
-          <a href="/">Portfolio</a>
+          <a href="#portfolio"
+             onClick={() => handlePageChange('Portfolio')} 
+             className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+          Portfolio
+          </a>
         </div>
         <div style={linkStyle}>
-          <a href="/">Contact</a>
+          <a href="#contact"
+             onClick={() => handlePageChange('Contact')} 
+             className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+          Contact
+          </a>
         </div>
         <div style={linkStyle}>
-          <a href="/">Resume</a>
+          <a href="#resume"
+             onClick={() => handlePageChange('Resume')} 
+             className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+          Resume
+          </a>
         </div>
       </section>
     </nav>
