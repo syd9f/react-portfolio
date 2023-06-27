1249b972489e6fcdbae1545321a61370402ca390
sydnief <sydniefarrell@gmail.com>
2023-06-26 23:03:34 -0400

    updated portfolio

---
+++ react-portfolio/src/components/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project = ({ title, description }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Project;
--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Project from '../Project';
 
 export default function Portfolio() {
-  return (
+    const projects = [
+        { id: 1, title: 'Password Generator', description: 'Generate a Random Password' },
+        { id: 2, title: 'Code Quiz', description: 'Practice your Coding Knowledge' },
+        { id: 3, title: 'Workday Scheduler', description: 'Plan your Workday' },
+      ];
+    return (
     <div>
-      <h1>Portfolio</h1>
-      <p>
-        This will be where the project cards display once I create them !!!
-        Yay!!!!!!
-      </p>
+        <h1>Portfolio</h1>
+        {projects.map((project) => (
+        <Project key={project.id} title={project.title} description={project.description} />
+        ))}
     </div>
-  );
+    );
 }
