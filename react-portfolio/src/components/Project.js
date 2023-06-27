410a95138aabf9206ed5cd36466bccb554b99836
sydnief <sydniefarrell@gmail.com>
2023-06-26 23:38:41 -0400

    updated image alt to title

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 
 const Project = ({ title, image, description }) => {
-    const imagePath = require(`./images/${image}`).default;
+    const imagePath = require(`../images/${image}`).default;
   return (
     <div>
       <h2>{title}</h2>
-      <img src={imagePath} alt="application" height="200" />
+      <img src={imagePath} alt={title} height="200" />
       <p>{description}</p>
     </div>
   );
