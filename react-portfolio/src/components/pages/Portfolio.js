8e5bd303365d85c8f51a25bf215920b39ce2a9a0
sydnief <sydniefarrell@gmail.com>
2023-06-26 23:36:07 -0400

    organized code

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -3,9 +3,21 @@ import Project from '../Project';
 
 export default function Portfolio() {
     const projects = [
-        { id: 1, title: 'Password Generator', image: 'password-generator.png', description: 'Generate a Random Password' },
-        { id: 2, title: 'Code Quiz', image: 'code-quiz.png', description: 'Practice your Coding Knowledge' },
-        { id: 3, title: 'Workday Scheduler', image: 'workday-scheduler.png', description: 'Plan your Workday' },
+        { 
+            id: 1, 
+            title: 'Password Generator', 
+            image: 'password-generator.png', 
+            description: 'Generate a Random Password' },
+        { 
+            id: 2, 
+            title: 'Code Quiz', 
+            image: 'code-quiz.png', 
+            description: 'Practice your Coding Knowledge' },
+        { 
+            id: 3, 
+            title: 'Workday Scheduler', 
+            image: 'workday-scheduler.png', 
+            description: 'Plan your Workday' },
       ];
     return (
     <div>
