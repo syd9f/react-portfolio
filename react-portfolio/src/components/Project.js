013e959c769f1085e16eb88310644d4eaa8f51b7
sydnief <sydniefarrell@gmail.com>
2023-06-26 23:18:07 -0400

    added images

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Project = ({ title, description }) => {
+
+const Project = ({ title, imageLink, description }) => {
   return (
     <div>
       <h2>{title}</h2>
+      <image src={imageLink}></image>
       <p>{description}</p>
     </div>
   );
